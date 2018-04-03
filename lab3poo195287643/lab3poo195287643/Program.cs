6b6114a2db8a82a1c8ee69e1f26abee670bc8257
2pacblanco <jadonoso2@miuandes.cl>
2018-04-03 17:08:26 -0300

    First commit for april 3 lab

---
+++ lab3poo195287643/lab3poo195287643/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3poo195287643
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
